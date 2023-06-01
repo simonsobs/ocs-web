93951638c193635967fe63c3ce37fe5ca2835bc0
Matthew Hasselfield <mhasself@users.noreply.github.com>
2023-06-01 14:38:20 -0400

    HWPSupervisor has summary and controls

--- src/panels/HWPSupervisor.vue
+++ src/panels/HWPSupervisor.vue
@@ -39,7 +39,75 @@
           />
         </OcsLightLine>
 
-        </div>
+        <OcsLightLine caption="Data sources">
+          <div v-for="item in dataSources" v-bind:key="item.name">
+            <OcsLight
+              :caption="item.name"
+              type="multi"
+              tip="Will show green/good when data source is not stale."
+              :value="item.ok"
+            />
+          </div>
+        </OcsLightLine>
+
+        <h2>HWP Summary</h2>
+
+        <OpReading
+          caption="PMX Summary"
+          :value="getMonThing('hwp_state', '!pmx_summary')"
+        />
+
+        <OpReading
+          caption="PID Summary"
+          :value="getMonThing('hwp_state', '!pid_summary')"
+        />
+
+        <OpReading
+          caption="UPS Summary"
+          :value="getMonThing('hwp_state', '!ups_summary')"
+        />
+
+        <OpReading
+          caption="Cryo Summary"
+          :value="getMonThing('hwp_state', '!therm_summary')"
+        />
+
+        <OcsLightLine
+          caption="Action Messages">
+          <OcsLight
+            caption="PMX"
+            tip="Supervisor system action recommendation: Green/good: 'ok'; Red/bad: 'stop'; Yellow/warning: 'no_data'."
+            type="multi"
+            :value="getIndicator('pmx')" />
+          <OcsLight
+            caption="Gripper"
+            tip="Supervisor system action recommendation: Green/good: 'ok'; Red/bad: 'stop'; Yellow/warning: 'no_data'."
+            type="multi"
+            :value="getIndicator('gripper')" />
+        </OcsLightLine>
+
+        <h2>Quick Actions</h2>
+
+        <form v-on:submit.prevent>
+          <div class="ocs_row">
+            <label>Stopping</label>
+            <button
+              :disabled="accessLevel < 1"
+              @click="quickAction('brake')">Active Brake</button>
+            <button
+              :disabled="accessLevel < 1"
+              @click="quickAction('pmx_off')">Power Off</button>
+          </div>
+          <div class="ocs_row">
+            <label>Set Spin Rate (Hz)</label>
+            <input type="text"
+                         v-model="this.ops.pid_to_freq.params.target_freq" />
+            <button
+              :disabled="accessLevel < 1"
+              @click="quickAction('pid_to_freq')">Set</button>
+          </div>
+        </form>
+      </div>
 
     </div>
 
@@ -152,6 +220,32 @@
       }
     },
     methods: {
+      getMonThing(k1, k2) {
+        let proc = this.ops.monitor.session.data;
+        if (proc && proc[k1]) {
+          switch (k2) {
+            case '!pmx_summary': {
+              return proc[k1]['pmx_voltage'] + " V / "
+                   + proc[k1]['pmx_current'] + " A / "
+                   + proc[k1]['pmx_source'];
+            }
+            case '!pid_summary': {
+              return 'Target ' + proc[k1]['pid_target_freq'] + ' Hz ; '
+                   + 'Current ' + proc[k1]['pid_current_freq'] + ' Hz.';
+            }
+            case '!ups_summary': {
+              return 'On ' + proc[k1]['ups_output_source'] + ' with '
+                   + proc[k1]['ups_estimated_minutes_remaining'] + ' mins left.';
+            }
+            case '!therm_summary': {
+              return 'T = ' + proc[k1]['temp'] + 'K (threshold: ' + proc[k1]['temp_thresh'] + ' K)';
+            }
+            default:
+              return proc[k1][k2];
+          }
+        }
+        return null;
+      },
       getIndicator(name) {
         let proc_stale_time = 3;  // Seems to be enough
 
@@ -172,8 +266,41 @@
           let stale = now - proc.data['timestamp'] > proc_stale_time;
           return (proc.status == 'running' && !stale);
         }
-        
+        else if (name == 'pmx' || name == 'gripper') {
+          let proc = this.ops.monitor.session.data;
+          if (proc && proc.actions) {
+            switch(proc.actions[name]) {
+              case 'ok':
+                return 'good';
+              case 'stop':
+                return 'bad';
+              case 'no_data':
+                return 'warning';
+            }
+          }
+        }
         return 'notapplic';
+      },
+      quickAction(name) {
+        window.ocs_bundle.ui_start_proc(this.address, name,
+                                            this.ops[name].params);
+      },
+    },
+    computed: {
+      dataSources() {
+        let src_stale_time = 10;  // Seems to be enough
+        let now = window.ocs_bundle.util.timestamp_now();
+        let proc = this.ops.monitor.session.data;
+        if (!proc)
+          return [];
+        let output = [];
+        if (proc.monitored_sessions) {
+          for (const [k, v] of Object.entries(proc.monitored_sessions)) {
+            let stale = (now - v.timestamp) > src_stale_time;
+            output.push({name: k, ok: !stale, agent_id: v.agent_id});
+          }
+        }
+        return output;
       },
     },
     mounted() {
