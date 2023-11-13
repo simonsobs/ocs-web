a462947da222a97b0890e3e144d6c4fa0eef234d
Matthew Hasselfield <mhasself@users.noreply.github.com>
2023-11-13 14:23:09 -0500

    OpDropdown handles v-model.boolean

--- src/components/OpDropdown.vue
+++ src/components/OpDropdown.vue
@@ -5,7 +5,7 @@
     <!-- options_style='array' -->
     <select class="ocs_double"
             :value="modelValue"
-            @input="$emit('update:modelValue', $event.target.value)"
+            @input="emitValue($event.target.value)"
             v-if="options_style == 'array'">
       <option v-for="opt in options" v-bind:key="opt" v-bind:value="opt">
         {{ opt }}
@@ -15,7 +15,7 @@
     <!-- options_style='object' -->
     <select class="ocs_double"
             :value="modelValue"
-            @input="$emit('update:modelValue', $event.target.value)"
+            @input="emitValue($event.target.value)"
             v-if="options_style == 'object'">
       <option v-for="(value, key) in options" v-bind:key="key" :value="key">
         {{ value }}
@@ -41,8 +41,18 @@
       },
       modelValue: {
         required: true},
+      modelModifiers: { default: () => ({}) },
     },
-}
+    //emits:
+    methods: {
+      emitValue(value) {
+        // Handle the "vmodel.boolean"
+        if (this.modelModifiers.boolean)
+          value = (value == "true");
+        this.$emit('update:modelValue', value);
+      },
+    },
+  }
 </script>
 
 <style scoped>
